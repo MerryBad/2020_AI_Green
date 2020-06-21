0abd960461cf5952676ccf8daffe0e862e0a7742
야옹 <49221790+MerryBad@users.noreply.github.com>
2020-06-21 13:38:38 +0900

    주석처리 완료

--- Detected_People_Near.py
+++ Detected_People_Near.py
@@ -1,7 +1,17 @@
-#================================================================
-#  To learn how to Develop Advance YOLOv4 Apps - Then check out:
-#  https://augmentedstartups.info/yolov4release
-#================================================================ 
+#############################################################
+## 인공지능과 기계학습
+##  조이름  : 소심조 (1조)
+##  작품명  : 초록이
+## 작품설명 : 보행자를 인식하여 가까이 오면 경고음을 발생
+## 실행환경 : Python 3.7.7
+##            CUDA 10.1
+##            cuDNN v7.6.5
+##            OpenCV 4.3.0
+##            Yolo v4
+##            Yolo>darknet>build>darknet>x64에서 실행
+##            Input과 Output 폴더를 생성해서 데이터입력과 저장
+##############################################################
+
 from ctypes import *
 import math
 import random
@@ -13,41 +23,20 @@ import darknet
 from itertools import combinations
 import winsound as ws
 
+## 호출시 비프음을 발생시킨다.
 def beepsound():
-    freq = 2000    # range : 37 ~ 32767
-    dur = 100     # ms
-    ws.Beep(freq, dur) # winsound.Beep(frequency, duration)
-    #print(beepsound())
+    freq = 2000        ## range : 37 ~ 32767
+    dur = 100          ## ms
+    ws.Beep(freq, dur) ## winsound.Beep(frequency, duration)
+    ## print(beepsound())
 
-
+## y축의 max와 min을 입력받아 크기를 계산
 def is_close(ymin, ymax):
-    """
-    #================================================================
-    # 1. Purpose : Calculate Euclidean Distance between two points
-    #================================================================    
-    :param:
-    p1, p2 = two points for calculating Euclidean Distance
+    dst = round(abs(ymax-ymin), 1) # 소수점 1자리까지 계산
+    return dst # 
 
-    :return:
-    dst = Euclidean Distance between two 2d points
-    """
-    dst = round(abs(ymax-ymin), 1)
-    #=================================================================#
-    return dst 
-
-
+## 중심 좌표를 직사각형 좌표로 변환한다.
 def convertBack(x, y, w, h): 
-    #================================================================
-    # 2.Purpose : Converts center coordinates to rectangle coordinates
-    #================================================================  
-    """
-    :param:
-    x, y = midpoint of bbox
-    w, h = width, height of the bbox
-    
-    :return:
-    xmin, ymin, xmax, ymax
-    """
     xmin = int(round(x - (w / 2)))
     xmax = int(round(x + (w / 2)))
     ymin = int(round(y - (h / 2)))
@@ -58,66 +47,67 @@ def convertBack(x, y, w, h):
 def cvDrawBoxes(detections, img):
     """
     :param:
-    detections = total detections in one frame
-    img = image from detect_image method of darknet
+    detections = 한 프레임에서 검출된 모든 객체
+    img = darknet(YOLO)을 사용한 이미지파일
 
     :return:
-    img with bbox
+    객체를 박스처리 한 이미지파일
     """
     #================================================================
-    # 3.1 Purpose : Filter out Persons class from detections and get 
-    #           bounding box centroid for each person detection.
+    # 3.1 Purpose : 탐지에서 Persons 클래스를 필터링하고 각 사람 감지에 대한 경계 상자 중심을 가져온다.
     #================================================================
-    if len(detections) > 0:  						# At least 1 detection in the image and check detection presence in a frame  
-        centroid_dict = dict() 						# Function creates a dictionary and calls it centroid_dict
-        objectId = 0								# We inialize a variable called ObjectId and set it to 0
-        for detection in detections:				# In this if statement, we filter all the detections for persons only
-            # Check for the only person name tag 
-            name_tag = str(detection[0].decode())   # Coco file has string of all the names
-            if name_tag == 'person':                
+    if len(detections) > 0:  						# 이미지에서 최소 1 회 감지 및 프레임에서 감지 유무 확인
+        centroid_dict = dict() 						# 함수는 사전을 만들고이를 centroid_dict라고 부릅니다.
+        objectId = 0							# ObjectId라는 변수를 초기화하고 0으로 설정합니다
+        for detection in detections:				        # 이 if 문에서 사람에 대한 모든 탐지를 필터링합니다.
+            # 태그가 사람일 경우만 확인 
+            name_tag = str(detection[0].decode())
+            if name_tag == 'person':
                 x, y, w, h = detection[2][0],\
                             detection[2][1],\
                             detection[2][2],\
-                            detection[2][3]      	# Store the center points of the detections
-                xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))   # Convert from center coordinates to rectangular coordinates, We use floats to ensure the precision of the BBox            
-                # Append center point of bbox for persons detected.
-                centroid_dict[objectId] = (int(x), int(y), xmin, ymin, xmax, ymax) # Create dictionary of tuple with 'objectId' as the index center points and bbox
-                objectId += 1 #Increment the index for each detection      
+                            detection[2][3]      	# 탐지된 객체의 중심점 저장
+                xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))   # 중심 좌표에서 직사각형 좌표로 변환, 부동 소수점을 사용하여 BBox의 정밀도를 보장한다.            
+                # 탐지 된 사람의 bbox 중심점을 추가
+                centroid_dict[objectId] = (int(x), int(y), xmin, ymin, xmax, ymax) # bbox의 중심점과 'objectId'를 사용하여 튜플 사전 만들기
+                objectId += 1 #각 탐지에 대한 갯수 증가      
     #=================================================================#
     
     #=================================================================
-    # 3.2 Purpose : Calc distance
+    # 3.2 Purpose : 크기 계산
     #=================================================================            	
-        red_zone_list = [] # List containing which Object id is in under threshold distance condition. 
-        for idx, box in centroid_dict.items(): # Get all the combinations of close detections, #List of multiple items - id1 1, points 2, 1,3
-            distance = is_close(box[5], box[3]) 			# Calculates the Euclidean distance
-            if distance > 55.0:						# Set our social distance threshold - If they meet this condition then..
+        red_zone_list = [] # 임계 값 조건에있는 오브젝트 ID를 포함하는 목록
+        for idx, box in centroid_dict.items(): # 감지된 모든 보행자의 정보
+            distance = is_close(box[5], box[3]) 			# 크기 계산
+            if distance > 40.0:						# 일정 크기 이상이라면
                 if idx not in red_zone_list:
-                    red_zone_list.append(idx)       #  Add Id to a list
+                    red_zone_list.append(idx)       #  리스트에 id를 추가한다.
         
         for idx, box in centroid_dict.items():  # dict (1(key):red(value), 2 blue)  idx - key  box - value
-            if idx in red_zone_list:   # if id is in red zone list
-                cv2.rectangle(img, (box[2], box[3]), (box[4], box[5]), (255, 0, 0), 2) # Create Red bounding boxes  #starting point, ending point size of 2
+            if idx in red_zone_list:   # red zone list에 id가 있다면
+                cv2.rectangle(img, (box[2], box[3]), (box[4], box[5]), (255, 0, 0), 2) # 빨간색 경계 상자 만들기
             else:
-                cv2.rectangle(img, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2) # Create Green bounding boxes
-		#=================================================================#
+                cv2.rectangle(img, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2) # 초록색 경계 상자 만들기
+	#=================================================================#
 
-		#=================================================================
-    	# 3.3 Purpose : Display Risk Analytics and Show Risk Indicators
+	#=================================================================
+    	# 3.3 Purpose : 정보 표시
     	#=================================================================        
-        text_detect = "Detect object : %s" % str(len(detections))
-        text_people = "Detect People : %s" % str(len(centroid_dict))
-        text_near = "Detect Near People : %s" % str(len(red_zone_list)) 			# Count People at Risk
+        text_detect = "Detect object : %s" % str(len(detections)        # 전체 감지된 숫자 
+        text_people = "Detect People : %s" % str(len(centroid_dict))    # 감지된 보행자 숫자
+        text_near = "Detect Near People : %s" % str(len(red_zone_list)) # 가까운 곳에 있는 보행자 숫자
+        # 문자열 생성 위치
         location_detect = (10,25)
         location_people = (10,50)
-        location_near = (10,75)												# Set the location of the displayed text
-        cv2.putText(img, text_detect, location_detect, cv2.FONT_HERSHEY_SIMPLEX, 1, (246,86,86), 2, cv2.LINE_AA)  # Display Text
-        cv2.putText(img, text_people, location_people, cv2.FONT_HERSHEY_SIMPLEX, 1, (246,86,86), 2, cv2.LINE_AA)  # Display Text
-        cv2.putText(img, text_near, location_near, cv2.FONT_HERSHEY_SIMPLEX, 1, (246,86,86), 2, cv2.LINE_AA)  # Display Text
+        location_near = (10,75)						    					
+                # 이미지   문자열           좌표              폰트형태          크기     색상     두께  선 종류
+        cv2.putText(img, text_detect, location_detect, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (246,86,86), 2, cv2.LINE_AA)  # Display Text
+        cv2.putText(img, text_people, location_people, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (246,86,86), 2, cv2.LINE_AA)  # Display Text
+        cv2.putText(img, text_near, location_near, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (246,86,86), 2, cv2.LINE_AA)      # Display Text
         #=================================================================#
-        # 3.4 Purpose : Call BeepSound When there is a person nearby 
+        # 3.4 Purpose : 가까운 곳에 보행자가 있다면 비프음 발생
         #=================================================================
-        if red_zone_list: # not empty list
+        if red_zone_list: # 리스트가 비어있지 않다면
             print(beepsound())
     return img
 
@@ -128,13 +118,11 @@ altNames = None
 
 
 def YOLO():
-    """
-    Perform Object detection
-    """
     global metaMain, netMain, altNames
-    configPath = "./cfg/yolov4.cfg"
-    weightPath = "./yolov4.weights"
-    metaPath = "./cfg/coco.data"
+    configPath = "./cfg/yolov4.cfg" # 환경설정파일 경로
+    weightPath = "./yolov4.weights" # 학습된 모델 경로
+    metaPath = "./cfg/coco.data"    # 코코 데이터 경로
+    # 각 경로에 파일이 존재하지 않으면
     if not os.path.exists(configPath):
         raise ValueError("Invalid config path `" +
                          os.path.abspath(configPath)+"`")
@@ -144,6 +132,7 @@ def YOLO():
     if not os.path.exists(metaPath):
         raise ValueError("Invalid data file path `" +
                          os.path.abspath(metaPath)+"`")
+    # 파일 열기
     if netMain is None:
         netMain = darknet.load_net_custom(configPath.encode(
             "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
@@ -169,46 +158,47 @@ def YOLO():
                     pass
         except Exception:
             pass
-    #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("./Input/test.mp4")
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-    new_height, new_width = frame_height // 2, frame_width // 2
-    # print("Video Reolution: ",(width, height))
+    #cap = cv2.VideoCapture(0) 웹캠 사용시 주석해제
+    for i in range(9):
+        cap = cv2.VideoCapture("./Input/"+str(i+1)+".mp4") # 1~9.mp4를 입력받는다
+        frame_width = int(cap.get(3))  # 영상의 너비
+        frame_height = int(cap.get(4)) # 영상의 높이
+        new_height, new_width = frame_height // 2, frame_width // 2 
+        # print("Video Reolution: ",(width, height)) #해상도 출력
 
-    out = cv2.VideoWriter(
-            "./Output/test_output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
-            (new_width, new_height))
-    
-    # print("Starting the YOLO loop...")
+        out = cv2.VideoWriter( # 결과물 저장
+                "./Output/"+str(i+1)+".avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
+                (new_width, new_height))
+        
+        # print("Starting the YOLO loop...")
 
-    # Create an image we reuse for each detect
-    darknet_image = darknet.make_image(new_width, new_height, 3)
-    
-    while True:
-        prev_time = time.time()
-        ret, frame_read = cap.read()
-        # Check if frame present :: 'ret' returns True if frame present, otherwise break the loop.
-        if not ret:
-            break
+        # 탐지 할 때마다 재사용하는 이미지를 만든다.
+        darknet_image = darknet.make_image(new_width, new_height, 3)
+        
+        while True:
+            prev_time = time.time()
+            ret, frame_read = cap.read()
+            # 프레임이 존재하면 true가 반환, 없으면 루프가 중단된다.
+            if not ret:
+                break
 
-        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb,
-                                   (new_width, new_height),
-                                   interpolation=cv2.INTER_LINEAR)
+            frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB) # BGR->GrayScale 변환
+            frame_resized = cv2.resize(frame_rgb, #프레임 크기 재조절
+                                       (new_width, new_height),
+                                       interpolation=cv2.INTER_LINEAR)
 
-        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+            darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes()) # 이미지를 바이트형식으로 가져옴
 
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-        image = cvDrawBoxes(detections, frame_resized)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        print(1/(time.time()-prev_time))
-        cv2.imshow('Detection near people', image)
-        cv2.waitKey(3)
-        out.write(image)
+            detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.75) # darknet을 이용한 객체탐지, 정확도 75% 이하는 버림
+            image = cvDrawBoxes(detections, frame_resized) # 네모박스 그리기
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # BGR에서 RGB로 색상 전환
+            print(1/(time.time()-prev_time)) # 진행시간 확인
+            cv2.imshow('Detection near people', image) # 이미지 출력
+            cv2.waitKey(3) # 키보드 입력 대기
+            out.write(image) # 이미지파일 저장
 
-    cap.release()
-    out.release()
+        cap.release() # cap객체 할당해제
+        out.release() # out객체 할당해제
     print(":::Video Write Completed")
 
 if __name__ == "__main__":
